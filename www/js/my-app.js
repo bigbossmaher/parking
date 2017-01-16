a8dc32f64dc741cfa471f49c420bd07b2df7265c
bigbossmaher <sammoudi.maher@gmail.com>
2017-01-16 19:02:32 +0100

    dd
    dd

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,7 +1,6 @@
 // Initialize your app
 var myApp = new Framework7({
-    material:true,
-    
+
 });
 
 // Export selectors engine
@@ -9,8 +8,10 @@ var $$ = Dom7;
 
 // Add view
 var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
 });
-/* ta9rib zaydain
+
 // Callbacks to run specific code for specific pages, for example for About page:
 myApp.onPageInit('about', function (page) {
     // run createContentPage func after link was clicked
@@ -47,24 +48,84 @@ function createContentPage() {
     );
 	return;
 }
-fin */
-// side config
- $$('.open-right-panel').on('click', function (e) {
+
+
+//side panels
+  $$('.open-left-panel').on('click', function (e) {
+        // 'left' position to open Left panel
+        myApp.openPanel('left');
+       map.setClickable( false );
+    });
+ 
+    $$('.open-right-panel').on('click', function (e) {
         // 'right' position to open Right panel
         myApp.openPanel('right');
-     map.setClickable( false );
+        map.setClickable( false );
     });
- $$('.open-left-panel').on('click', function (e) {
-        // 'right' position to open Right panel
-        myApp.openPanel('left');
-     map.setClickable( false );
-    });
-//autocomplet
-$$('.address-text').on('focusin', function (e) {
-        // 'right' position to open Right panel
+$$('.panel-right').on('panel:closed', function () {
+    myApp.alert('Left panel is closing!');
+});
+
+
+//autocomplete part
+$$('#myplace').on('focusin', function (e) {
     map.setClickable( false );
+    $$('#myplace').val('');
     });
-$$('.address-text').on('blur', function (e) {
-        // 'right' position to open Right panel
+$$('#myplace').on('blur', function (e) {
     map.setClickable( true );
+    });
+
+
+//marquer ma voiture
+$$('.save-storage-data').on('click', function() {
+      var onSuccess = function(location) {
+          var storedData = myApp.formStoreData('localisation_du_voiture', {
+    'lat': location.latLng.lat,
+     'lng': location.latLng.lng    
+        
+  });
+  map.addMarker({
+    'position': location.latLng,
+    'title': "Votre Voiture est ici"
+  }, function(marker) {
+    marker.showInfoWindow();
+  });
+   myApp.addNotification({
+        title: 'Parking Tunisie',
+        message: 'La localisation de votre voiture est bien enregistré.'
+    });       
+};
+
+var onError = function(msg) {
+  alert("Erreur : on n'a pas pu localiser votre voiture");
+};
+map.getMyLocation(onSuccess, onError);   
+});
+$$('.get-storage-data').on('click', function() {
+  var storedData = myApp.formGetData('localisation_du_voiture');
+  if(storedData) {
+    myApp.alert(JSON.stringify(storedData));
+  }
+  else {
+    alert('There is no stored data for this form yet. Try to change any field')
+  }
+    map.setZoom(7);
+});
+
+//facebook sidebar button
+$$('#facook_page').on('click', function (e) {
+    startApp.set({ /* params */
+    "action": "ACTION_VIEW",
+    "uri": "fb://facewebmodal/f?href=https://www.facebook.com/parkingtn/"
+}).start(); 
+    });
+
+//notre sitewbe button
+$$('#notre_siteweb').on('click', function (e) {
+    startApp.set({ /* params */
+    "action": "ACTION_VIEW",
+    "uri": "http://www.parking.tn/v1/"
+}).start();
+
     });
